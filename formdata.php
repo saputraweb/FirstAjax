1a955cca7bc7b45ade02472c2f397ef7f88cf278
willy <willyganjarsaputra@gmail.com>
2014-02-26 08:41:36 +0700

    Form Data

---
+++ formdata.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>First Ajax Tutor</title>
+</head>
+<script type="text/javascript">
+
+</script>
+<body>
+<table>
+	<tr>
+		<td>NIP :</td>
+	</tr>
+	<tr>
+		<td>
+			<select name="nip" id="">
+				<option value="123456">Wildan Hermansyah</option>
+				<option value="234567">Carli Rido</option>
+				<option value="345678">Danim Riandi</option>
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>Alamat :</td>
+	</tr>
+	<tr>
+		<td><textarea name="alamat" id="alamat" cols="30" rows="10"> </textarea></td>
+	</tr>
+</table>
+</body>
+</html>
